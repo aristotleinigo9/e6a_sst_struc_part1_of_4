13c240d09e9665244fc4aeae712e657f1c0cd59d
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2023-08-22 15:07:57 +0300

    first commit

---
+++ myscript.js
@@ -0,0 +1,619 @@
+var myQuestions = [
+  {
+    question: "1. The doctors ________ work in this hospital are very professional.",
+    answers: {
+      a: 'whose',
+      b: 'who',
+      c: 'whom',
+      d: 'which'
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "2.	Sami still has __________ sugar and rice in his store.",
+    answers: {
+      a: 'few',
+      b: 'a number of',
+      c: 'many',
+      d: 'some'
+
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "3.	The workers __________ for the bus were very tired.",
+    answers: {
+      a: 'waited',
+      b: 'waiting',
+      c: 'wait',
+      d: 'waits'
+
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "4.	I bought the book __________ John told me about.",
+    answers: {
+      a: 'which',
+      b: 'whose',
+      c: 'who',
+      d: 'where'
+
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "5.	I liked the food __________ Mrs. Williams cooked last Friday.",
+    answers: {
+      a: 'whose',
+      b: 'whom',
+      c: 'that',
+      d: 'where'
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "6. They need to drink __________ coffee.",
+    answers: {
+      a: 'a lot',
+      b: 'a little',
+      c: 'a few',
+      d: 'several'
+
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "7.	I don’t know where __________ ",
+    answers: {
+      a: 'he lives',
+      b: 'is he living',
+      c: 'does he live',
+      d: 'did he live'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "8. This is the office __________ the clerk usually types the reports.",
+    answers: {
+      a: 'which',
+      b: 'that',
+      c: 'where',
+      d: 'when'
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "9.	He insisted that he __________ his friend in the hospital.",
+    answers: {
+      a: 'visit',
+      b: 'visited',
+      c: 'visits',
+      d: 'visiting'
+
+    },
+    correctAnswer: 'a'
+  },
+  
+  {
+    question: "10. There are __________ notebooks on the desk.",
+    answers: {
+      a: 'several',
+      b: 'a great deal',
+      c: 'much',
+      d: 'plenty'
+
+    },
+    correctAnswer: 'a'
+  },
+  
+  {
+    question: "11.	It is essential that we __________ poor people.",
+    answers: {
+      a: 'helped',
+      b: 'help',
+      c: 'helps',
+      d: 'helping'
+
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "12.	He will never forget the year __________ his best friend died",
+    answers: {
+      a: 'which',
+      b: 'from which',
+      c: 'at which',
+      d: 'in which'
+
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "13.	Do you know who __________ yesterday?",
+    answers: {
+      a: 'comes',
+      b: 'will come',
+      c: 'came',
+      d: 'is coming'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "14.	Salem is living in a city __________ in the northern part of Saudi Arabia",
+    answers: {
+      a: 'locating',
+      b: 'locates',
+      c: 'locate',
+      d: 'located'
+
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "15.	__________ students are very clever.",
+    answers: {
+      a: 'Both',
+      b: 'Much',
+      c: 'A great deal of',
+      d: 'Plenty'
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "16.	I wonder whether or not __________ us tomorrow.",
+    answers: {
+      a: 'will he visit',
+      b: 'he visit',
+      c: 'he will visit',
+      d: 'is he going to visit'
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "17.	January is the month __________ the weather is usually the coldest.",
+    answers: {
+      a: 'at which',
+      b: 'on which',
+      c: 'when',
+      d: 'which'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "18.	I am trying to remember how many books__________.",
+    answers: {
+      a: 'will buy',
+      b: 'will we buy',
+      c: 'we are going to buy',
+      d: 'must we buy'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "19.	Mary likes to drive her __________ car.",
+    answers: {
+      a: "father's",
+      b: "fathers' ",
+      c: "fathers's",
+      d: 'fathers'
+
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "20.	The police __________ ten criminals since August.",
+    answers: {
+      a: "arrests",
+      b: "has arrested",
+      c: "have arrested",
+      d: 'was arrested'
+
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "21.	The doctor told me __________ fish or shrimp.",
+    answers: {
+      a: "don't eat",
+      b: "not eat",
+      c: "doesn't",
+      d: "not to eat"
+
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "22.	I visited the house in __________ Ali lives.",
+    answers: {
+      a: "where",
+      b: "which",
+      c: "when",
+      d: "whose"
+
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "23.	They did not know what __________",
+    answers: {
+      a: "to do",
+      b: "do they do",
+      c: "did they do",
+      d: "must they do"
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "24.	The __________ toys are usually made in bright colors.",
+    answers: {
+      a: "childrens'",
+      b: "childrens's",
+      c: "children's",
+      d: "childrens"
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "25.	I don’t like this pencil. Please, give me __________ one.",
+    answers: {
+      a: "others'",
+      b: "the another",
+      c: "the others",
+      d: "another"
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "26.	She said, “I have to take the book.” She told me that she __________ the book.",
+    answers: {
+      a: "has to take",
+      b: "has taken",
+      c: "had to take",
+      d: "had taken"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "27.	I always meet the man __________ son studies medicine in England.",
+    answers: {
+      a: "whom",
+      b: "that",
+      c: "whose",
+      d: "who"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "28.	I will invite __________ would like to come to the party.",
+    answers: {
+      a: "whatever",
+      b: "whomever",
+      c: "whenever",
+      d: "whoever"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "29.	I saw a lot of pictures, only a few of __________ were clear.",
+    answers: {
+      a: "which",
+      b: "whose",
+      c: "whom",
+      d: "who"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "30.	The mechanic to __________ you spoke last night was very skillful.",
+    answers: {
+      a: "whose",
+      b: "that",
+      c: "which",
+      d: "whom"
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "31.	This is the__________ suitcase.",
+    answers: {
+      a: "boys",
+      b: "boy's",
+      c: "boy",
+      d: "boys's"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "32.	I have a __________ boy.",
+    answers: {
+      a: "five year",
+      b: "five-years",
+      c: "five-year-old",
+      d: "five-years-old"
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "33.	I don’t like __________.",
+    answers: {
+      a: "vegetable soup",
+      b: "soup's vegetable",
+      c: "vegetable's soup",
+      d: "vegetables soup"
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "34.	I sold my __________ villa.",
+    answers: {
+      a: "7 room",
+      b: "7 rooms",
+      c: "7 room's",
+      d: "7-room"
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: "35.	I bought	__________ furniture.",
+    answers: {
+      a: "too many",
+      b: "too much",
+      c: "too",
+      d: "much very"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "36.	I eat__________ ice cream.",
+    answers: {
+      a: "too many",
+      b: "many",
+      c: "plenty of",
+      d: "plenty"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "37.	In big cities, there is __________ pollution.",
+    answers: {
+      a: "several",
+      b: "a great deal of",
+      c: "a few",
+      d: "much of"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "38.	The girl is wearing __________ jewelry.",
+    answers: {
+      a: "several",
+      b: "a few",
+      c: "too many",
+      d: "some"
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "39.	__________ student has a book",
+    answers: {
+      a: "All",
+      b: "Every",
+      c: "Some",
+      d: "Much"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "40.	Khobar is one of the __________ that I like very much.",
+    answers: {
+      a: "cities",
+      b: "city's",
+      c: "city",
+      d: "cities'"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "41.	The experiment they did__________ very exciting.",
+    answers: {
+      a: "were",
+      b: "was",
+      c: "are",
+      d: "have been"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "42.	Growing indoor plants__________ my hobby.",
+    answers: {
+      a: "is",
+      b: "are",
+      c: "were",
+      d: "was"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "43.	A number of students __________ late for school yesterday.",
+    answers: {
+      a: "was",
+      b: "is",
+      c: "were",
+      d: "are"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "44.	One of the boys __________ a car. The others don’t.",
+    answers: {
+      a: "owns",
+      b: "own",
+      c: "have",
+      d: "owner"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "45.	Half of the money __________ to me.",
+    answers: {
+      a: "belong",
+      b: "belonging",
+      c: "belongs",
+      d: "is belonging"
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "46.	The rich __________ a lot of money.",
+    answers: {
+      a: "have",
+      b: "has",
+      c: "owns",
+      d: "possesses"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "47.	Each employee should do __________ level best.",
+    answers: {
+      a: "his",
+      b: "he",
+      c: "him",
+      d: "he's"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "48.	My  wonderful family always __________ me.",
+    answers: {
+      a: "supports",
+      b: "support",
+      c: "supportive",
+      d: "suporting"
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "49.	You must do this homework by __________.",
+    answers: {
+      a: "yourself",
+      b: "ourselves",
+      c: "myself",
+      d: "themselves"
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "50.	I received two checks.  One is from my father and the __________ is from my uncle.",
+    answers: {
+      a: "others",
+      b: "another",
+      c: "the others",
+      d: "other"
+    },
+    correctAnswer: 'd'
+  },
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+  function showQuestions(questions, quizContainer){
+    var output = [];
+    var answers;
+    for(var i=0; i<questions.length; i++){
+      answers = [];
+      for(choices in questions[i].answers){
+
+        answers.push(
+          '<label>'
+            + '<input type="radio" name="question'+i+'" value="'+choices+'">'+ choices + '. '+ questions[i].answers[choices] + 
+            '</label>'
+            +'<br>'
+        );
+
+      }
+      output.push(
+        '<div class="question">'  + questions[i].question + '</div>' + '<div class="answers">'  + answers.join('') + '<br>' + '</div>'
+      );
+    }
+    quizContainer.innerHTML = output.join('');
+
+  }
+
+
+  function showResults(questions, quizContainer, resultsContainer){
+    var answerContainers = quizContainer.querySelectorAll('.answers');
+    var userAnswer = '';
+    var numCorrect = 0;
+  
+    for(var i=0; i<questions.length; i++){
+
+      userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+      if(userAnswer===questions[i].correctAnswer){
+
+        numCorrect++;
+        answerContainers[i].style.color = 'green';
+      }
+      else{
+        answerContainers[i].style.color = 'red';
+      }
+    }
+    
+   resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length + " or " + (numCorrect/questions.length * 100).toFixed(2) + "%"; 
+   
+  }
+
+
+  showQuestions(questions, quizContainer);
+  
+  submitButton.onclick = function(){
+    showResults(questions, quizContainer, resultsContainer);
+  }
+
+}
